6b64493616bb35654f5cd0eef23045b4a42121a9
Anton Kurkov <KurkovAV@polymetal.ru>
2025-01-10 11:55:08 +0300

    Fixed Linter errors.

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,25 +1,23 @@
+import { CSSProperties } from 'react';
 import { TailSpin } from 'react-loader-spinner';
 
 
-const styles = {
-  overlay: {
-    position: 'fixed', // Фиксируем на экране
-    top: 0,
-    left: 0,
-    width: '100vw', // Растягиваем на всю ширину окна
-    height: '100vh', // Растягиваем на всю высоту окна
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Полупрозрачный черный фон
-    display: 'flex', // Используем flexbox для центрирования
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 1000, // Устанавливаем высокий z-index, чтобы перекрывать другие элементы
-  },
+const overlayStyle: CSSProperties = {
+  position: 'fixed', // Фиксируем на экране
+  top: 0,
+  left: 0,
+  width: '100vw', // Растягиваем на всю ширину окна
+  height: '100vh', // Растягиваем на всю высоту окна
+  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Полупрозрачный черный фон
+  display: 'flex', // Используем flexbox для центрирования
+  justifyContent: 'center',
+  alignItems: 'center',
+  zIndex: 1000, // Устанавливаем высокий z-index, чтобы перекрывать другие элементы
 };
-
 
 function Spinner():JSX.Element{
   return (
-    <div style={styles.overlay}>
+    <div style={overlayStyle}>
       <TailSpin
         height="80"
         width="80"
