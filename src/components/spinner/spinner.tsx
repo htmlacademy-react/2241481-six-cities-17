7afc18802f190403a21b8f8bb726a959134a957a
Anton Kurkov <KurkovAV@polymetal.ru>
2025-01-29 10:24:44 +0300

    Fixed examiner's remarks.

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,23 +1,9 @@
-import { CSSProperties } from 'react';
 import { TailSpin } from 'react-loader-spinner';
-
-
-const overlayStyle: CSSProperties = {
-  position: 'fixed', // Фиксируем на экране
-  top: 0,
-  left: 0,
-  width: '100vw', // Растягиваем на всю ширину окна
-  height: '100vh', // Растягиваем на всю высоту окна
-  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Полупрозрачный черный фон
-  display: 'flex', // Используем flexbox для центрирования
-  justifyContent: 'center',
-  alignItems: 'center',
-  zIndex: 1000, // Устанавливаем высокий z-index, чтобы перекрывать другие элементы
-};
+import { spinnerStyle } from '../../types/style-types';
 
 function Spinner():JSX.Element{
   return (
-    <div style={overlayStyle}>
+    <div style={spinnerStyle}>
       <TailSpin
         height="80"
         width="80"
